73b2860248aad9b3eda41c2bf5b7d35e9e8c39a2
Edmundo Pichardo <mundix@Edmundos-MacBook-Pro.local>
2016-02-22 06:36:05 -0400

    Se configuro y agrego attachment avatar a user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :roles
   belongs_to :users
+
+  has_attached_file :avatar, styles: { medium: "700x500#", small: "350x250>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222023410) do
+ActiveRecord::Schema.define(version: 20160222103510) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20160222023410) do
     t.boolean  "gender"
     t.date     "birth"
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
